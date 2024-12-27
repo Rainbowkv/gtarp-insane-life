7dec7ea624d0dd21e7ee1287d1fda25d6827e643
Rainbowkv <981512226@qq.com>
2024-12-28 00:13:28 +0800

    配置参数修改以测试是否不饿死渴死

--- qb-core/client/loops.lua
+++ qb-core/client/loops.lua
@@ -15,7 +15,7 @@ CreateThread(function()
             if (QBCore.PlayerData.metadata['hunger'] <= 0 or QBCore.PlayerData.metadata['thirst'] <= 0) and not (QBCore.PlayerData.metadata['isdead'] or QBCore.PlayerData.metadata['inlaststand']) then
                 local ped = PlayerPedId()
                 local currentHealth = GetEntityHealth(ped)
-                if currentHealth > 5 then  -- 超过5滴血才扣血
+                if currentHealth > 105 then  -- 超过105滴血才扣血, 100滴血就是死了
                     local decreaseThreshold = math.random(5, 10)
                     SetEntityHealth(ped, currentHealth - decreaseThreshold)
                 end
