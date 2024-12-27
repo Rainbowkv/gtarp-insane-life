18957735a04a95dd43a286e1b405f62ff2b7664f
Rainbowkv <981512226@qq.com>
2024-12-28 00:37:11 +0800

    正确理解参数后的重新配置饥渴参数

--- qb-core/client/loops.lua
+++ qb-core/client/loops.lua
@@ -2,7 +2,7 @@ CreateThread(function()
     while true do
         local sleep = 0
         if LocalPlayer.state.isLoggedIn then
-            sleep = (1000 * 60) * QBCore.Config.UpdateInterval
+            sleep = (1000 * 60) * QBCore.Config.UpdateInterval  -- 不断饥饿渴的速度
             TriggerServerEvent('QBCore:UpdatePlayer')
         end
         Wait(sleep)
@@ -18,9 +18,10 @@ CreateThread(function()
                 if currentHealth > 105 then  -- 超过105滴血才扣血, 100滴血就是死了
                     local decreaseThreshold = math.random(5, 10)
                     SetEntityHealth(ped, currentHealth - decreaseThreshold)
+                    QBCore.Functions.Notify("饥饿渴正在损坏您的健康...", "warning")
                 end
             end
         end
-        Wait(QBCore.Config.StatusInterval)
+        Wait(QBCore.Config.StatusInterval)  -- 饥饿渴时，掉血的速度
     end
 end)
