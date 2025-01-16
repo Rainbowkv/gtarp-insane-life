89334683297d6d861fec4ee44e86faaa2aa7fc4c
Rainbowkv <981512226@qq.com>
2025-01-16 17:35:07 +0800

    M5加入

--- qb-core/shared/vehicles.lua
+++ qb-core/shared/vehicles.lua
@@ -761,6 +761,7 @@ local Vehicles = {
     -- customized
     { model = 'su7',             name = 'SU7',                           brand = '小米',            price = 100000000,  category = '定制车',      type = 'automobile', shop = 'luxury' },
     { model = 'rs721',           name = 'RS7',                           brand = '奥迪',            price = 100000000,  category = '定制车',      type = 'automobile', shop = 'none' },
+    { model = 'oycm5cs',         name = 'M5',                            brand = '宝马',            price = 100000000,  category = '定制车',      type = 'automobile', shop = 'none' },
     -- special
     { model = 'voltic2',         name = '火箭伏尔蒂克',                  brand = '科尔',             price = 9830400, category = '超级跑车',         type = 'automobile', shop = 'none' },
     { model = 'oppressor',       name = '压迫者',                        brand = '佩加西',         price = 9999999, category = '摩托车',    type = 'bike',       shop = 'none' },
