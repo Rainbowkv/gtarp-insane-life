582da4c59726ddca5740cfdaaf119dda6ecf7749
Rainbowkv <981512226@qq.com>
2025-05-24 23:44:48 +0800

    现在可以饿死

--- qb-core/client/loops.lua
+++ qb-core/client/loops.lua
@@ -15,10 +15,13 @@ CreateThread(function()
             if (QBCore.PlayerData.metadata['hunger'] <= 0 or QBCore.PlayerData.metadata['thirst'] <= 0) and not (QBCore.PlayerData.metadata['isdead'] or QBCore.PlayerData.metadata['inlaststand']) then
                 local ped = PlayerPedId()
                 local currentHealth = GetEntityHealth(ped)
-                if currentHealth > 110 then  -- 超过110滴血才扣血, 100滴血就是死了
+                -- if currentHealth > 110 then  -- 超过110滴血才扣血, 100滴血就是死了
+                if currentHealth > 100 then  -- 超过100滴血才扣血, 100滴血以下已经死了
                     local decreaseThreshold = math.random(5, 10)
                     SetEntityHealth(ped, currentHealth - decreaseThreshold)
-                    QBCore.Functions.Notify("饥饿渴正在损坏您的健康...", "warning")
+                    if currentHealth < 120 then
+                        QBCore.Functions.Notify("您即将因为饥饿或口渴而死亡...", "error")
+                    end
                 end
             end
         end
